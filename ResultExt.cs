e73e449ef3434d1583c4b92b383ab91eed9e3c7b
dengyakui <dengyakui@leansoftx.com>
2022-07-09 12:53:16 +0800

    compare bewteen MemoryStream and FileFufferingWriteStream when write stream to body

--- ResultExt.cs
+++ ResultExt.cs
@@ -1,13 +1,18 @@
 using System.Xml.Serialization;
 
+using Microsoft.AspNetCore.WebUtilities;
 namespace Microsoft.AspNetCore.Http;
 
 public static class ResultsExtension
 {
-  public static IResult Xml<T>(this Microsoft.AspNetCore.Http.IResultExtensions resultExtension, T obj)
-  {
-     return new XmlResult<T>( obj);
-  }
+    public static IResult Xml<T>(this Microsoft.AspNetCore.Http.IResultExtensions resultExtension, T obj)
+    {
+        return new XmlResult<T>(obj);
+    }
+    public static IResult XmlV2<T>(this Microsoft.AspNetCore.Http.IResultExtensions resultExtension, T obj)
+    {
+        return new XmlResultV2<T>(obj);
+    }
 }
 
 
@@ -36,5 +41,33 @@ public class XmlResult<T> : IResult
 
         httpContext.Response.ContentType = "application/xml";
         await ms.CopyToAsync(httpContext.Response.Body);
+    }
+}
+
+
+
+public class XmlResultV2<T> : IResult
+{
+    // Create the serializer that will actually perform the XML serialization
+    private static readonly XmlSerializer Serializer = new(typeof(T));
+
+    // The object to serialize
+    private readonly T _result;
+
+    public XmlResultV2(T result)
+    {
+        _result = result;
+    }
+
+    public async Task ExecuteAsync(HttpContext httpContext)
+    {
+        // NOTE: best practice would be to pull this, we'll look at this shortly
+        using var ms = new FileBufferingWriteStream();
+
+        // Serialize the object synchronously then rewind the stream
+        Serializer.Serialize(ms, _result);
+
+        httpContext.Response.ContentType = "application/xml";
+        await ms.DrainBufferAsync(httpContext.Response.Body);
     }
 }
